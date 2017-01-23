ee215a0cd14db50f4e5c66f65af7dfc44de2498b
joel <joeltburton@gmail.com>
2017-01-22 20:18:29 -0600

    implemented number random number generator, random fortunes, and the new fortune cookie button

--- main.py
+++ main.py
@@ -15,11 +15,54 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def randomNumbers(howmany,max_num):
+    numbers = []
+    for num in range(0,howmany):
+        n = random.randint(1,max_num+1)
+        numbers.append(n)
+    return numbers
+
+def randomFortune():
+    # list of possible fortunes
+    fortunes = [
+    "You will meet someone of great interest very soon.",
+    "You will face a test of great skill in the coming weeks.",
+    "You should explore more. Who knows what awaits you.",
+    "People are very interested in you."
+    ]
+
+    # randomly select a fortune by choosing an index on the list
+    idx = random.randint(0,len(fortunes)-1)
+
+    # return the fortune
+    return fortunes[idx]
+
+
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello butts!')
 
-app = webapp2.WSGIApplication([
+    def get(self):
+
+        lucky_nums = randomNumbers(6,99)
+        fortune = randomFortune()
+
+        header = "<h1>Fortune Cookie</h1>"
+        fortune_string = "<p>Your fortune:  <em>" + fortune + "</em></p>"
+        num_string = "<p>Your lucky numbers: <strong>"+str(lucky_nums)+"</strong></p>"
+
+        cookie_button  = "<a href='.'><button>Another fortune cookie please!</button></a>"
+
+        content = header + fortune_string + num_string + cookie_button
+
+        self.response.write(content)
+
+
+
+routes = [
     ('/', MainHandler)
-], debug=True)
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
